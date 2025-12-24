3dff35cfbb780ca5a0c08d51f611480d3fce6a02
aspandyar <aspandyart@gmail.com>
2025-12-24 22:30:03 +0500

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,20 @@ import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap"
 import icon from "astro-icon";
-// Remove the staticAdapter import - not needed for static output
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static', // This is enough for static sites
-  // Remove the adapter line completely
-  site: "https://aspandyar.github.io/aspandyar",
-  base: process.env.BASE_PATH || "/",
-  integrations: [preact(), icon(), sitemap({
-    filter: (page) =>
-      !page.includes("/blog/tags") &&
-      !page.includes("/blog/techs"),
-  }),],
-
+  output: 'static',
+  site: 'https://aspandyar.github.io',
+  base: '/aspandyar', // Remove this if your repo is USERNAME.github.io
+  integrations: [
+    preact(), 
+    icon(), 
+    sitemap({
+      filter: (page) =>
+        !page.includes("/blog/tags") &&
+        !page.includes("/blog/techs"),
+    })
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
